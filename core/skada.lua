0d3be9a5058832ed5312cd19e55ac99a017d48f1
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-11-19 19:50:39 +0100

    Temp skada stuff

---
+++ core/skada.lua
@@ -0,0 +1,7 @@
+local addon = oPanel
+local frame = SkadaBarWindowSkada
+
+frame:ClearAllPoints()
+frame:SetPoint("TOP", addon, 0, 6)
+frame:SetPoint("BOTTOM", addon, 0, 8)
+frame:SetPoint("LEFT", addon, 550, 0)
